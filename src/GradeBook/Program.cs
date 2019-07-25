c84e87274f7e59382bd7e37239a4361cad7e0704
15sora2 <stgordon19@gmail.com>
2019-07-24 19:46:03 -0500

    creating repository

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            var grades = new List<double>() {12.7, 12.7, 12.7};
+            grades.Add(12.1);
+            
+            var result = 0.0;
+            foreach(double number in grades){
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+            //N1 is a format specifier that prints the result through 1 place after the decimal
+
+            if(args.Length > 0){
+            Console.WriteLine($"Hello {args[0]} !");
+            //string interpolation: using $ and {array[index]}
+            }
+            else{
+            Console.WriteLine("Hello!");
+        }
+        }
+        
+        }
+    }
+    
